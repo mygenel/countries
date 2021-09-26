3805ae9e200329e24efc256029a64473391bc0ea
mygenel <myg26@drexel.edu>
2021-09-26 00:30:20 -0400

    created navigation with router

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
 import NavBar from './components/NavBar';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <h1>Home</h1>
+          </Route>
+          <Route exact path="/name">
+            <h1>Name</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- countries/src/components/NavBar.jsx
+++ countries/src/components/NavBar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const NavBar = () => (
   <>
     <Navbar bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#home">Country Information</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Country Information</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">By Name</Nav.Link>
-          <Nav.Link href="#features">By Language</Nav.Link>
-          <Nav.Link href="#pricing">About</Nav.Link>
+          <Nav.Link as={Link} to="/name">By Name</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
